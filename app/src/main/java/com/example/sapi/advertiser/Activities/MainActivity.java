253b9f86fa70bef95ae2f85547dd0bcc60b8c567
orsolya_zolcsak <orsolya_zolcsak@gmail.com>
2017-11-02 19:29:12 +0200

    alap

--- app/src/main/java/com/example/sapi/advertiser/Activities/MainActivity.java
+++ app/src/main/java/com/example/sapi/advertiser/Activities/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.sapi.advertiser;
+package com.example.sapi.advertiser.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.sapi.advertiser.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
